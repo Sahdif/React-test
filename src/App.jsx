4f5b64408b3a01b59126888a8a993db1b4af2712
Sahdif <sahdif.islam3@gmail.com>
2022-11-30 23:12:45 +1100

    react-test

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './components/Nav'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="red">
+      <Nav /> 
+      <Header />
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div>
+            <h1>Learn React</h1>
+            <p>React is a Javascript library made by facebook that allows you to create apps effciently</p>
+            <h3>Let's Begin!</h3>
+        </div>
+    );
+}
+
+export default Nav;
